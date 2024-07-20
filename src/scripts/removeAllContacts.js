a996a9769941a1b3781baf1b973012d944ec84b6
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-07-20 15:54:58 +0300

    step 7 done

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -3,7 +3,6 @@ import * as fs from 'node:fs/promises';
 
 export const removeAllContacts = async () => {
   try {
-    // const data = fs.readFile(PATH_DB, 'utf-8');
     await fs.writeFile(PATH_DB, JSON.stringify([]));
   } catch (error) {
     console.error(error);
