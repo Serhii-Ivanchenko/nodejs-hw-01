559de85fa4cdb515d37b7a45a19bb1032c234691
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-07-20 13:40:34 +0300

    step 2 done

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,18 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    for (let index = 0; index < number; index++) {
+      await fs.appendFile(
+        PATH_DB,
+        JSON.stringify(createFakeContact(), undefined, 2),
+      );
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 generateContacts(5);
